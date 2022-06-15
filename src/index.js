18967a5ea8260b32ddc86b1774efe139c5ce1839
NunoSempere <nuno.sempere@protonmail.com>
2022-06-15 15:05:29 -0400

    tweak: save progress

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 // IMPORTS
 import * as fs from "fs";
 import { mergeSort } from "./mergeSort.js";
-import { findPaths } from "./findPaths.js";
+import { findDistancesFromAllElementsToAllReferencePoints } from "./findPaths.js";
 import { aggregatePaths } from "./aggregatePaths.js";
 
 // DEFS
 const inputLinksFilePath = "./input/input-links.json";
 const inputListFilePath = "./input/input-list.json";
 const outputFilePath = "./output/output.json";
+
+// HELPERS
+
+const findElementPosition = (name, orderedList) => {
+  let node = orderedList.find((node) => node.name == name);
+  return node.position;
+};
 
 // MAIN
 async function main() {
@@ -23,8 +30,25 @@ async function main() {
   // const list = [...new Set([...sources, ...targets])];
   // process in mergeSort
 
+  // Merge sort
   let mergeSortOutput = mergeSort({ list, links });
   console.log("Output: ");
   console.log(mergeSortOutput);
+
+  // find Paths
+  let nodes = mergeSortOutput.map((element, i) => ({
+    ...element,
+    position: i,
+  }));
+  const linksWithPosition = links.map((link) => ({
+    ...link,
+    sourceElementPosition: findElementPosition(link.source, nodes),
+    targetElementPosition: findElementPosition(link.target, nodes),
+  }));
+  let paths = await findDistancesFromAllElementsToAllReferencePoints({
+    nodes,
+    links: linksWithPosition,
+  });
+  console.log(paths);
 }
 main();
